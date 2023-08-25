25c16803a3a2f2083c0b4cc965559fc81e52b32b
irismq8125 <mauquy123@gmail.com>
2023-08-25 16:28:09 +0700

    Add project files.

---
+++ Controllers/TongHaiSo/TongHaiSoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebMvc.Controllers.TongHaiSo
+{
+    public class TongHaiSoController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(int soNguyen1, int soNguyen2)
+        {
+            ViewData["SoNguyen1"] = soNguyen1;
+            ViewData["SoNguyen2"] = soNguyen2;
+            ViewData["KetQua"] = soNguyen1 + soNguyen2;
+            return View();
+        }
+
+
+
+
+
+
+        //[HttpPost]
+        //public IActionResult Index(int soNguyen1, int soNguyen2)
+        //{
+        //    try
+        //    {
+        //        ViewData["SoNguyen1"] = soNguyen1;
+        //        ViewData["SoNguyen2"] = soNguyen2;
+        //        ViewData["KetQua"] = soNguyen1 / soNguyen2;
+        //        ViewData["Message"] = "";
+        //    }
+        //    catch (DivideByZeroException dex)
+        //    {
+        //        ViewData["Message"] = "Khong dc phep chia cho 0";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        ViewData["Message"] = ex.Message;
+        //    }
+        //    return View();
+        //}
+    }
+}
